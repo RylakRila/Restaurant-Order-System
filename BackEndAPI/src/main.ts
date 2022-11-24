f9b99d8880d0bf53d98ec4d193d6c3a063fd1c68
RylakRila <kylvark@gmail.com>
2022-11-24 10:33:00 -0500

    change /api to GlobalPrefix

--- BackEndAPI/src/main.ts
+++ BackEndAPI/src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const port = process.env.PORT || 3000;
   
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
   await app.listen(port);
 }
 bootstrap();
