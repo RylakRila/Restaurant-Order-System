2a80e971d6e2d78e8b7adc997a1cb23c7b0c7da7
Kai <bono6415@gmail.com>
2022-12-07 01:42:03 -0500

    Enable to pass the CORS

--- BackEndAPI/src/main.ts
+++ BackEndAPI/src/main.ts
@@ -6,6 +6,11 @@ async function bootstrap() {
   const port = process.env.PORT || 3000;
   
   const app = await NestFactory.create(AppModule);
+  /*
+  * @https://docs.nestjs.com/security/cors
+  */ 
+  app.enableCors();
+  
   app.setGlobalPrefix('api');
   await app.listen(port);
 }
