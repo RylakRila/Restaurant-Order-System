5407d83ef51ef99126507518dc78ebd3e187fb41
RylakRila <kylvark@gmail.com>
2023-07-22 16:50:28 -0400

    add server url env in main.js

--- FrontEnd/src/main.js
+++ FrontEnd/src/main.js
@@ -7,7 +7,7 @@ import { BootstrapVue, IconsPlugin} from 'bootstrap-vue'
 
 Vue.prototype.$axios = axios;
 Vue.prototype.qs = qs;
-Vue.prototype.$backEndUrl = 'http://localhost:3000/api/'
+Vue.prototype.$backEndUrl = process.env.SERVER_URL || 'http://localhost:3000/api/';
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
